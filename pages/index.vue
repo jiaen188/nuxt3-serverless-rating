1a526eee6ae6cfdb4821ad60305f34de986a82e7
gulifan <gulifan@dxy.cn>
2023-12-20 19:45:37 +0800

    feat: 样式布局

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,24 @@
 
 <template> 
 <div id="app">
-    <h1>hello world</h1>
-    <nuxt-link to="/ranks">ranks</nuxt-link>
+    <div class="emoji-wrapper overflow-hidden row-span-2 p-1" ref="emojiWrapper">
+        <div v-for="emoji in emojis" :key="emoji" class="emoji w-18 h-18 flex justify-center items-center snap-y mb-2 text-7xl">
+            {{ emoji }}
+        </div> 
+    </div>
+    <div class="text-center leading-8">Your vote: ?</div>
+    <div class="rating flex-row-reverse justify-center items-center" @change="onChange">
+        <input type="radio" class="mask mask-star-2 w-10 h-10" v-for="item in 5" :class="{active: item == myRate}" @click="myRate = item" :value="6-item" v-model="myRate">
+    </div>
+    <div class="col-span-2 text-center pt-1"><nuxt-link to="/ranks">ranks</nuxt-link></div>
 </div>
 </template>
 
 <script setup lang="ts">
+const emojis = ['🙃', '😞', '🧐', '😀', '🥳']
+const emojiWrapper = ref<HTMLDivElement>()
+const onChange = () => {
 
+}
+const myRate = ref<number>(0)
 </script>
